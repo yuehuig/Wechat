c6333160d170e7676f39d37c7ea2c9bc410e5885
519856549@qq.com <519856549@qq.com>
2016-03-14 22:04:47 +0800

    first

---
+++ NewWeChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewWeChat
+//
+//  Created by MornChan on 16/2/14.
+//  Copyright © 2016年 MornChan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ NewWeChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewWeChat
+//
+//  Created by MornChan on 16/2/14.
+//  Copyright © 2016年 MornChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
